b35e99b0d9475933f1099b31f4fc1a36f4f7230f
pq070pq <pq070pq@gmail.com>
2026-09-22 02:27:46 +0300

    fix: use production holiday radar scheduler

--- app/jobs.py
+++ app/jobs.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from .config import settings
 from .db import SessionLocal, Subscription
 from .telegram import send_message, bot_api
-from .channel_radar import broadcast_closed_market, stock_radar_enabled
+from .holiday_radar import publish_holiday_radar
 from .timeutil import utcnow, aware
 
 async def expiry_cycle():
